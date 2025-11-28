651179a5b647dac703f8d5551ea79a16511f6e3d
saurabh kumar <saurabhkumarycse@gmail.com>
2025-11-28 15:30:22 +0530

    My test how pull work

--- src/App.jsx
+++ src/App.jsx
@@ -169,6 +169,7 @@ export default function App() {
         </div>
       ) : (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+          <h1>change tracking</h1>
           {data?.products?.slice(0, 5)?.map((product) => (
             <div
               key={product.id}
